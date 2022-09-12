b107fc68c9dd1b1205ae02497a25919730dd8e8c
Botlhale <Botlhalersehume@icloud.com>
2022-09-12 02:00:20 -0700

    redo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the put function 
-*Description: using the main function 
-* this progarm prints "Programming is like building a multilingual puzzle 
-* Return: 0
-*/
+ * main - print the string in the put function 
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
