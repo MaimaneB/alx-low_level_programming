8eb53d203ce3650ccfff4df9071a3a0d35bc2924
Botlhale <Botlhalersehume@icloud.com>
2022-09-12 06:48:30 -0700

    att4

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,19 +16,19 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-if (n % 10 > 5) 
+if (n > 0) 
 {
-	printf("last digit of %i is %i and is positive\n", n, n % 10);
+	printf("%i is positive\n", n);
 }
 
-else if (n % 10 == 0)
+else if (n == 0)
 {
-	printf("last digit of %i is %i and is zero\n", n, n % 10);
+	printf("%i is zero\n", n);
 }
 
-else (n % -10 < 0)
+else (n < 0)
 {
-	printf("last digit of %i is %i and  negative\n", n); 
+	printf("%i is negative\n", n); 
 }
 
 return (0);
