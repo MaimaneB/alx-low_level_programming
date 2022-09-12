680b9e616ef9abaa440b13d4c924e7a62d0c5836
Botlhale <Botlhalersehume@icloud.com>
2022-09-12 02:43:35 -0700

    correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,12 @@
 /**
  * main - print the string in the put function
  *
- * Description: using the main function
+ * Description: prints "with proper grammar, but the outcome is a piece of art"
  *
- * this programming prints "with proper grammar, but the outcome is a piece of art"
  * Return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
