7611c91859c89cd3e04e2788ed7d6db1221bfdfc
Botlhale <Botlhalersehume@icloud.com>
2022-09-13 02:56:47 -0700

    base

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
  * main - 'print all single digit numbers of base 10 starting from 0'
+ *
  * Description: use the main function
  * the program prints "all single digit numbers of base 10 starting from 0"
  * Return; 0
