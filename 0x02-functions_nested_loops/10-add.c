24eabf37c16d39861b1bed7834cfd0b30b32ced0
Botlhale <Botlhalersehume@icloud.com>
2022-09-14 12:03:14 -0700

    adding

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,7 +5,7 @@
  * add - adding two integers
  * @num1 - 'first integer to be added'.
  * @num2 - 'second integer to be added'.
- * Description: "this program observe the sum of two integers"
+ * Description: 'this program adds num1 and num2'
  * Return: the sum of the two integers.
  */
 int add(int num1, int num2)
