b9d43a0c4f5e5534a318bbb932d06db954c2d1d6
Botlhale <Botlhalersehume@icloud.com>
2022-09-14 02:56:17 -0700

    int _islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *_islower - function checks lowercase characters 
+ *
+ * @c: int to be used for argument 
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
