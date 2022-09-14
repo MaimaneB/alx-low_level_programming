9fa13778669b513d9813442c7a19337dbdd5419e
Botlhale <Botlhalersehume@icloud.com>
2022-09-14 03:40:59 -0700

    6-abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - The function computes the absolute value of an int 
+ * @c: is the int to be used for the argument of a function
+ * Return: 0 
+ */
+int _abs(int)
+{
+	if (c > 0 || c == 0)
+	{
+		return (c);
+	}
+else
+return (c * -1);
+}
