42dae581e8e9c778de2b449e174b914fd71dad8c
Botlhale <Botlhalersehume@icloud.com>
2022-09-14 05:56:25 -0700

    jacks day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main"
+#include <stdio.h>
+/**
+ * jack_bauer - this function prints every minute of jack_bauer's day 
+ * @n: beginning at 00:00 to 23:59, the hour loop counts hours while the min loop counts mins
+ * and: resets the mins
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int hours = 0;
+	int minutes = 0;
+	int minutes_remainder;
+	int hours_remainder;
+
+	while (hours <= 23)
+	{
+	while (minutes <= 59)
+		{
+			hours_remainder = hours % 10;
+			_putchar(hours / 10 + '0');
+			_putchar(hours_remainder + '0');
+			_putchar(':');
+			_putchar(minutes / 10 + '0');
+			_putchar(minutes_remainder + '0');
+			minutes++;
+			_putchar('\n');
+		}
+	hours++;
+	minutes = 0;
+	}
+}
