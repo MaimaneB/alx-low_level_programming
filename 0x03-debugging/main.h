0da285ab01b7db40ef3fa51470ef8eed4a0e3610
Botlhale <Botlhalersehume@icloud.com>
2022-09-16 00:12:13 -0700

    0-main

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#ifndef Main.h
+#define Main.h 
+#include <stdio.h>
+
+postitive_or_negative(int i);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
+
+#endif /* Main.h */
