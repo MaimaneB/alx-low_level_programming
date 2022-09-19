2d1edec1121fdaa7f264cdc72b1ea9b90c1bad48
Botlhale <Botlhalersehume@icloud.com>
2022-09-19 11:00:37 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _strlen - returns the strings length
+* @s: string
+* Return: returns the length as an integer;
+*/
+
+int _strlen(char *s)
+{
+	int leng = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
