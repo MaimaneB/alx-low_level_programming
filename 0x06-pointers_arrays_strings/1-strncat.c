957f37c72512430023310ab67fb86345cd4b7f4e
Botlhale <Botlhalersehume@icloud.com>
2022-09-22 11:04:06 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
 *_strncat - concatenate two strings and add inputted number of bytes
-*@dest: string to be appended upon 
-*@src: string to be completed at end of dest 
+*@dest: string to be appended upon
+*@src: string to be completed at end of dest
 *@n: integer parameter to compare index
-*Return: returns new concatenated string 
+*Return: returns new concatenated string
 */
 
 char *_strncat(char *dest, char *src, int n);
 {
 
-	int index = 0,dest_len = 0;
+	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
