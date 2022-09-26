305a77dcf6b976aec2bfd9389da779c60b5b16e4
Botlhale <Botlhalersehume@icloud.com>
2022-09-26 00:30:51 -0700

    1-isdigit.c

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -4,13 +4,13 @@
 /**
 * _isdigit - checks whether the character is a digit or not 
 * @c: tested character 
-* Return: Always 0
+* Return: return 1 if c is a digit otherwise 0
 */
 
 int _isdigit(int c)
 {
-	if ((c >= 48) && (c <= 57))
-		return (1);
-	else
-		return (0);
+if ((c >= 48) && (c <= 57))
+return (1);
+else
+return (0);
 }
