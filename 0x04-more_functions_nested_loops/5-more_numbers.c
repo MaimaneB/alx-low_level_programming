189107760637123611d0d51b69d8e1319b09fcfd
Botlhale <Botlhalersehume@icloud.com>
2022-09-26 01:26:54 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h> 
+
+/**
+ * more_numbers - function to print 10x the number from 0 - 14
+ * _putchar only 3 times 
+ * Return: 0 - 14 * 10 followed by a new line 
+ */
+
+void more_numbers(void)
+{
+	int i, ro; 
+
+	for (ro = 0; ro < 10; ro++)
+
+	{
+		for (i = 0; i <= 14; i++)
+
+		{
+			if (i >= 10)
+
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
+
+		}
+		_putchar('\n');
+	}
+
+}
+
