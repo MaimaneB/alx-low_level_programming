62019c0baff66d065f72a2db53823e184b07b5d3
MaimaneB <Botlhalersehume@gmail.com>
2023-03-23 00:31:40 -0700

    Print string in Rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - This prints the string in reverse
+ * @s - string to be printed 
+ */
+
+_print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
